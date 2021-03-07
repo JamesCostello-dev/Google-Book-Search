883a43dfee7a0842401d3977d907bcd16e49a3bb
JamesCostello-dev <jcotab@gmail.com>
2021-03-07 15:57:31 -0700

    update app.js with apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,34 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloServer, ApolloClient } from '@apollo/client';
+
+const client = new ApolloClient({
+  req: op => {
+    const token = localStorage.getItem('id_token')
+    op.setCount({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: 'graphql'
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloClient client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloClient>
   );
 }
 
