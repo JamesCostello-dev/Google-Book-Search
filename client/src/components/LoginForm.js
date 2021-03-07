e07a19a873cb1b9970aad726cd898b572c8021ef
JamesCostello-dev <jcotab@gmail.com>
2021-03-07 16:21:11 -0700

    refactor signup and login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,10 +2,13 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { LOGIN_USER } from '../utils/mutations'
+import { useMutation } from '@apollo/client'
 
 const LoginForm = () => {
+  const [login] = useMutation(LOGIN_USER)
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,7 +29,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await login(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { CREATE_USER } from '../utils/mutations'
+import { useMutation } from '@apollo/client'
 
 const SignupForm = () => {
+  const [createUser] = useMutation(CREATE_USER);
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
