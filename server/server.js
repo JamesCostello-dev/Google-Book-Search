a84e33a5ac0fa40226240fdd1ee1518aceb1075a
JamesCostello-dev <jcotab@gmail.com>
2021-03-08 10:53:22 -0700

    fix error and redeploy

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require('./schemas')
+const { typeDefs, resolvers } = require('./schemas');
 //const routes = require('./routes');
 
 const app = express();
